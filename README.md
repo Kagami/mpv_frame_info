22eeaaa670eef6a618e06c32f0a022e8b6fd5c6f
Kagami Hiiragi <kagami@genshiken.org>
2015-09-25 00:32:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# mpv_frame_info
+
+Simple Lua script for mpv to display frame info, similar to ffdshow's OSD.
+Might be useful in case of e.g. screenshot comparisions.
+
+![](https://raw.githubusercontent.com/Kagami/mpv_frame_info/master/example.png)
+
+## Usage
+
+Put `frame_info.lua` to `~/.config/mpv/scripts/` or `~/.mpv/scripts/` directory
+to autoload the script or load it manually with `--script=<path>`.
+
+The script is binding itself to `i` (however, not overriding your own bindings)
+and can therefore be invoked with this key. You can create different
+bindings by adding `<yourkey> script_binding frame_info` to `input.conf`.
+
+Press `i` to toggle info displaying, press `Ctrl+s` to make the screenshot with info.
+
+## License
+
+mpv_frame_info - Show frame info with mpv
+
+Written in 2015 by Kagami Hiiragi <kagami@genshiken.org>
+
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
---
+++ frame_info.lua
@@ -0,0 +1,60 @@
+local assdraw = require "mp.assdraw"
+local options = require "mp.options"
+
+local info_active = false
+local o = {
+    font_size = 10,
+    font_color = "00FFFF",
+    border_size = 1.0,
+    border_color = "000000",
+}
+options.read_options(o)
+
+function get_formatting()
+    return string.format(
+        "{\\fs%d}{\\1c&H%s&}{\\bord%f}{\\3c&H%s&}",
+        o.font_size, o.font_color,
+        o.border_size, o.border_color
+    )
+end
+
+function timestamp(duration)
+    local hours = duration / 3600
+    local minutes = duration % 3600 / 60
+    local seconds = duration % 60
+    return string.format("%02d:%02d:%02.03f", hours, minutes, seconds)
+end
+
+function get_info()
+    return string.format(
+        "%sName: %s\\NTime: %s",
+        get_formatting(),
+        mp.get_property("filename"),
+        timestamp(mp.get_property_native("time-pos"))
+    )
+end
+
+function render_info()
+    ass = assdraw.ass_new()
+    ass:pos(0, 0)
+    ass:append(get_info())
+    mp.set_osd_ass(0, 0, ass.text)
+end
+
+function clear_info()
+    mp.set_osd_ass(0, 0, "")
+end
+
+function toggle_info()
+    if info_active then
+        mp.unregister_event(render_info)
+        clear_info()
+    else
+        -- TODO: Rewrite to timer + pause/unpause handlers.
+        mp.register_event("tick", render_info)
+        render_info()
+    end
+    info_active = not info_active
+end
+
+mp.add_key_binding("i", mp.get_script_name(), toggle_info)
